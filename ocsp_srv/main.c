75543ea2742b4008b0203238c65687711780fc15
jykim <jykim@dongbangsystem.com>
2019-11-14 15:02:40 +0900

    Base work

--- ocsp_srv/main.c
+++ ocsp_srv/main.c
@@ -1,7 +1,34 @@
 #include <stdio.h>
 
-int main()
+#include "js_process.h"
+#include "ocsp_srv.h"
+
+int OCSPService( JThreadInfo *pThInfo )
 {
-    printf("Hello World!\n");
     return 0;
 }
+
+#if 0
+int main()
+{
+    JProcInit   sProcInit;
+
+    memset( &sProcInit, 0x00, sizeof(sProcInit));
+
+    sProcInit.nCreateNum = 2;
+    JS_PRC_logInit( "./log", "ocsp", 3 );
+    JS_PRC_initRegister( &sProcInit );
+
+    JS_PRC_register( "JS_OCSP", NULL, 9010, 4, NULL, OCSPService );
+
+    JS_PRC_start();
+
+    return 0;
+}
+
+#else
+int main()
+{
+    return 0;
+}
+#endif
