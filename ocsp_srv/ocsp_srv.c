463be0b3f15f70cfcd2911ae8b5d2d991259cdd8
jykim <jykim@ranix.co.kr>
2023-12-06 18:56:58 +0900

    Thread 처리 지원 중

--- ocsp_srv/ocsp_srv.c
+++ ocsp_srv/ocsp_srv.c
@@ -713,8 +713,9 @@ int main( int argc, char *argv[] )
     LI( "OCSP Server initialized succfully" );
 
     JS_THD_logInit( "./log", "ocsp", 2 );
-    JS_THD_registerService( "JS_OCSP", NULL, g_nPort, 4, NULL, OCSP_Service );
-    JS_THD_registerService( "JS_OCSP_SSL", NULL, g_nSSLPort, 4, NULL, OCSP_SSL_Service );
+    JS_THD_registerService( "JS_OCSP", NULL, g_nPort, 4, OCSP_Service );
+    JS_THD_registerService( "JS_OCSP_SSL", NULL, g_nSSLPort, 4, OCSP_SSL_Service );
+//    JS_THD_registerAdmin( NULL, g_nPort + 100 );
     JS_THD_serviceStartAll();
 
     return 0;
